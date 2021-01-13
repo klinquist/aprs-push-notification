0f045af905eb4747094ec90f50e6ec50a60b5752
Kris Linquist <kris@linquist.net>
2021-01-13 07:50:27 -0800

    Support for always receiving a beacon from specific calls

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 ## APRS Beacon Push Notification
 
-This is a nodejs script that will send you a push notification if someone sends a beacon within X miles of a location.  An account with the "pushover" push notification service is required. https://pushover.net
+This is a nodejs script that will send you a push notification:
+* If a defined list of callsigns sends a beacon
+* If any beacons are received within X miles of a location.  
+
+
+An account with the "pushover" push notification service is required. https://pushover.net
 
 
 Rename `config.json.example` to `config.json` and edit it.  Below is the format, commented:
@@ -15,7 +20,9 @@ Rename `config.json.example` to `config.json` and edit it.  Below is the format,
             "reportCloserThanDistanceMiles": 1.0,  //Send a push notification if there are any beacons closer than this many miles 
             "pushoverUser": "XXX",                 //Pushover user key
             "pushoverToken": "XXX",                //Pushover app token
-            "exclude": ["XXXXX-10"]                //Exclude these call signs
+            "prefix": "",                          //Prefix to include in the push notification (in case you are monitoring >1 location)
+            "exclude": ["XXXXX-10"]                //Exclude these call signs from location-based push notifications
+            "include": ["XXXXX", "XXXXX-2"]        //Always receive push notifications of beacons from these calls regardless of location
         }
     ]
 }
@@ -24,7 +31,7 @@ Rename `config.json.example` to `config.json` and edit it.  Below is the format,
 "beacons" is an array, so you can support multiple users/locations.
 
 
-By default the script will exclude duplicate messages sent within 10 minutes.
+By default the script will exclude duplicate location-based messages sent within 10 minutes and exclude duplicate "include" messages sent within 30 minutes.
 
 
 Run `npm install` to install dependencies, then `node index.js`.  Run under pm2 or similar for persistence.
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const hashPrecision = 3;
-const dedupeMinutes = 10;
+const nearbyDedupeMinutes = 10;
+const includesDedupeMinutes = 30;
 
 
 const aprs = require('aprs-parser');
@@ -10,7 +11,8 @@ const geolib = require('geolib');
 const NodeCache = require('node-cache');
 const moment = require('moment');
 
-const pushCache = new NodeCache({ stdTTL: dedupeMinutes*60});
+const nearbyCache = new NodeCache({ stdTTL: nearbyDedupeMinutes * 60 });
+const includesCache = new NodeCache({ stdTTL: includesDedupeMinutes*60});
 
 const get = (p, o) => {
     if (!Array.isArray(p)) p = p.split('.');
@@ -26,13 +28,23 @@ try {
 }
 
 const geoObj = {};
+const includeObj = {};
 
-console.log('Building geohash cache...');
+console.log('Building geohash & include cache...');
 for (let i = 0; i < pushUsers.beacons.length; i++){
     const hash = ngeohash.encode(pushUsers.beacons[i].myLat, pushUsers.beacons[i].myLong, hashPrecision);
     console.log(`Assigning hash "${hash}" to array element ${i}`);
-    geoObj[hash] = i;
+    if (typeof geoObj[hash] == 'undefined') geoObj[hash] = [];
+    geoObj[hash].push(i);
+    if (pushUsers.beacons[i].include) {
+        for (let p = 0; p < pushUsers.beacons[i].include.length; p++){
+            console.log(`Assigning include "${pushUsers.beacons[i].include[p]}" to array element ${i}`);
+            if (typeof includeObj[pushUsers.beacons[i].include[p]] == 'undefined') includeObj[pushUsers.beacons[i].include[p]] = [];
+            includeObj[pushUsers.beacons[i].include[p]].push(i);
+        }
+    }
 }
+
 
 const getCall = event => {
     if (event.from.ssid) {
@@ -44,14 +56,14 @@ const getCall = event => {
 
 const getRadio = event => {
     if (event && event.data && event.data.radio) {
-        return ` (${event.data.radio})`;
+        return `(${event.data.radio})`;
     }
     return '';
 };
 
 const getComment = event => {
     if (event && event.data && event.data.comment) {
-        return ` (${event.data.comment.trim()})`;
+        return `(${event.data.comment.trim()})`;
     }
     return '';
 };
@@ -60,23 +72,51 @@ const getRoundedDistance = distance => {
     return Math.round(distance * 10) / 10;
 };
 
-const getMsg = (msg, distance, direction, event) => {
+const getMsg = (msg, prefix, distance, direction, event) => {
     const now = moment().format('MMM Do h:mm:ss A');
+    const pfx = prefix || '';
+    let rtn;
     if (direction) {
-        return `${now}: ${msg}: ${getCall(event)} is ${getRoundedDistance(distance)} mi ${direction}${getRadio(event)}${getComment(event)}`;
+        rtn =  `${now}: ${pfx} ${msg}: ${getCall(event)} is ${getRoundedDistance(distance)} mi ${direction} ${getRadio(event)} ${getComment(event)}`;
     } else {
-        return `${now}: ${msg}: ${getCall(event)} is ${getRoundedDistance(distance)} mi ${getRadio(event)}${getComment(event)}`;
+        rtn = `${now}: ${pfx} ${msg}: ${getCall(event)} is ${getRoundedDistance(distance)} mi ${getRadio(event)} ${getComment(event)}`;
     }
+    rtn = rtn.replace(/\s\s+/g, ' ');
+    return rtn;
+};
+
+
+const sendPush = (user, token, msg, cb) => {
+    const push = new Push({ user, token });
+    push.send({
+        message: msg,
+        priority: 1
+    }, (err) => {
+        if (err) console.log('Error sending push notification: ' + err);
+        return cb(err);
+    });
+};
+
+const processInclude = (lat, long, event, currentElement) => {
+    let distance = geolib.getDistance(
+        { latitude: lat, longitude: long },
+        { latitude: currentElement.myLat, longitude: currentElement.myLong }
+    );
+    if (includesCache.get(getCall(event))) {
+        console.log('Discarding duplicate');
+    }
+    const direction = geolib.getCompassDirection({ latitude: currentElement.myLat, longitude: currentElement.myLong }, { latitude: lat, longitude: long });
+    distance = distance * 0.000621371; //m to mi
+    const msg = getMsg('Beacon', currentElement.prefix, distance, direction, event);
+    console.log(msg);
+    sendPush(currentElement.pushoverUser, currentElement.pushoverTokenUser, msg, (err, res) => {
+        if (!err) includesCache.set(getCall(event), new Date().toISOString());
+    });
 
 };
 
-const processEvent = event => {
-    const lat = get('data.latitude', event);
-    const long = get('data.longitude', event);
-    if (!lat || !long) return;
-    const location = ngeohash.encode(lat, long, hashPrecision);
-    if (typeof geoObj[location] == 'undefined') return;
-    const currentElement = pushUsers.beacons[geoObj[location]];
+
+const processNearby = (lat, long, event, location, currentElement) => {
     let distance = geolib.getDistance(
         { latitude: lat, longitude: long },
         { latitude: currentElement.myLat, longitude: currentElement.myLong }
@@ -84,27 +124,40 @@ const processEvent = event => {
     const direction = geolib.getCompassDirection( { latitude: currentElement.myLat, longitude: currentElement.myLong },{ latitude: lat, longitude: long });
     distance = distance * 0.000621371; //m to mi
     if (currentElement.exclude.indexOf(getCall(event)) > -1) {
-        console.log(getMsg('Excluded beacon', distance, direction, event));
-    } else if (pushCache.get(getCall(event))) {
-        console.log(getMsg('Duplicate beacon', distance, direction, event));
+        console.log(getMsg('Excluded beacon', currentElement.prefix,distance, direction, event));
+    } else if (nearbyCache.get(getCall(event))) {
+        console.log(getMsg('Duplicate beacon', currentElement.prefix,distance, direction, event));
     } else if (distance < currentElement.reportCloserThanDistanceMiles) {
-        const push = new Push({ user: currentElement.pushoverUser, token: currentElement.pushoverTokenUser });
-        const msg = getMsg('APRS beacon', distance, direction, event);
+        const msg = getMsg('Nearby beacon', currentElement.prefix,distance, direction, event);
         console.log(msg);
-        push.send({
-            message: msg,
-            priority: 1
-        }, (err) => {
-            if (err) {
-                console.log('Error sending push notification: ' + err);
-            } else {
-                pushCache.set(getCall(event), new Date().toISOString());
-            }
+        sendPush(currentElement.pushoverUser, currentElement.pushoverTokenUser, msg, (err, res) => {
+            if (!err) nearbyCache.set(getCall(event), new Date().toISOString());
         });
     } else {
-        console.log(getMsg(`Nearby geohash ${location} but not close enough to send a push notification`, distance, direction, event));
+        console.log(getMsg(`Nearby geohash ${location} but not close enough to send a push notification`, currentElement.prefix, distance, direction, event));
+    }
+};
+
+const processEvent = event => {
+    const lat = get('data.latitude', event);
+    const long = get('data.longitude', event);
+    if (!lat || !long) return;
+    const location = ngeohash.encode(lat, long, hashPrecision);
+    if (includeObj[event.from.call]) {
+        includeObj[event.from.call].forEach((el) => {
+            processInclude(lat, long, event, pushUsers.beacons[el]);
+        });
+    }
+    if (includeObj[`${event.from.call}-${event.from.ssid}`]) {
+        includeObj[`${event.from.call}-${event.from.ssid}`].forEach((el) => {
+            processInclude(lat, long, event, pushUsers.beacons[el]);
+        });
     }
 
+    if (typeof geoObj[location] == 'undefined') return;
+    geoObj[location].forEach((el) => {
+        processNearby(lat, long, event, location,  pushUsers.beacons[el]);
+    });
 };
 
 
